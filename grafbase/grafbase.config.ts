8ede33d0ac7f9b0b804b004105ab070f2378ba30
Jayesh Patel <jb24pate@uwaterloo.ca>
2023-07-04 19:31:41 -0400

    First commit

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,17 @@
+import { g, auth, config } from "@grafbase/sdk";
+
+// Welcome to Grafbase!
+// Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
+// Integrate Auth
+// https://grafbase.com/docs/auth
+//
+// const authProvider = auth.OpenIDConnect({
+//   issuer: process.env.ISSUER_URL ?? ''
+// })
+//
+// Define Data Models
+// https://grafbase.com/docs/database
+
+export default config({
+  schema: g,
+});
